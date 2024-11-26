9254c16adee0ab29965672658fb0fc9a67daa7e1
Luca <luca.masera79@gmail.com>
2024-11-26 16:46:14 +0100

    Update rules.mk
    Removed leds configuration.
    Removed wpm because not used anymore. 
    Removed mouse keys because this function has been almost never used.

--- rules.mk
+++ rules.mk
@@ -1,12 +1,7 @@
 # COMMAND_ENABLE = no
-MOUSEKEY_ENABLE = yes
-WPM_ENABLE = yes
 
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 # We are not going to use them, they have unsuable positions
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
-
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = WS2812
