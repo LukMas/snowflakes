4b4f325508585e7f4dc273400d34ff7e5d1d9555
Luca Masera <luca.masera@aau.at>
2024-12-04 12:22:23 +0100

    Removed some layers; configure gui l/r; removed esc/gui

--- keymap.c
+++ keymap.c
@@ -21,19 +21,18 @@ enum layers {
     _QWERTY = 0,
     _NUMBERs,
     _FUNCTIONs,
-    _MEDIA,
-    _NAVI
+    _MEDIA
 };
 
 // layers modificators
 #define GO_NUMBs    MO(_NUMBERs)
 #define GO_FUNCs    MO(_FUNCTIONs)
 #define GO_MEDIA    TT(_MEDIA)
-#define NAVI_TAB    LT(_NAVI, KC_TAB)
+#define MEDIA_TAB   LT(_MEDIA, KC_TAB)
 
 // left and right control when held, left and right parenthesis when tap
-#define LCL_LPRT    KC_LCPO
-#define RCL_RPRT    KC_RCPC
+#define LCL_LPRT    SC_LCPO
+#define RCL_RPRT    SC_RCPC
 
 // left alt when held, left and right brackets when tap - the same ALT, so on the other layers doesn't mess with the position of the other modifiers
 #define LALT_LBK    LALT_T(KC_LBRC)
@@ -46,8 +45,7 @@ enum layers {
 // right alt when held, space and enter when tap - the same ALT, so on the other layers doesn't mess with the position of the other modifiers
 // #define LALT_SPC    LALT_T(KC_SPC)
 // #define LALT_ENT    LALT_T(KC_ENT)
-
-#define ESC_GUI     GUI_T(KC_ESC)
+//#define ESC_GUI     GUI_T(KC_ESC)
 
 // window and screen movement
 #define WDW_MLS HYPR(KC_LEFT) // move left screen
@@ -73,104 +71,83 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer: QWERTY
  *
  *,---------------------------------------------.                              ,---------------------------------------------.
- *|NAVI Tab |   Q  |   W  |   E   |   R  |   T  |                              |   Y  |   U  |   I   |  O   |  P   | Esc Gui |
+ *|Media Tab|   Q  |   W  |   E   |   R  |   T  |                              |   Y  |   U  |   I   |  O   |  P   |  Esc    |
  *|---------+------+------+-------+------+------|                              |------+------+-------+------+------+---------|
  *| Ctrl (  |   A  |   S  |   D   |   F  |   G  |                              |   G  |   H  |   J   |  K   | ; :  | Ctrl )  |
  *|---------+------+------+-------+------+------+-------------.  ,-------------+------+------+-------+------+------+---------|
  *| Shift { |   Z  |   X  |   C   |   V  |   B  | = +  | App  |  | Ins  | - _  |   N  |   M  |  , <  | . >  | / ?  | Shift } |
  *`-----------------------+-------+------+------+------+------|  |------+------+------+------+-------+-----------------------'
- *                        | MEDIA | LAlt | FUNC | Space| DEL  |  | Bcksp| Enter| NUMB | RAlt |  NO   |
+ *                        | L GUI | LAlt | FUNC | Space| DEL  |  | Bcksp| Enter| NUMB | RAlt | R GUI |
  *                        |       |  [   |      |      |      |  |      |      |      |  ]   |       |
  *                         -----------------------------------    -----------------------------------
  */
     [_QWERTY] = LAYOUT(
-     NAVI_TAB ,KC_Q    ,KC_W     ,KC_E     ,KC_R     ,KC_T                       ,                        KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,  ESC_GUI,
+     MEDIA_TAB,KC_Q    ,KC_W     ,KC_E     ,KC_R     ,KC_T                       ,                        KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,   KC_ESC,
      LCL_LPRT, KC_A    ,KC_S     ,KC_D     ,KC_F     ,KC_G                       ,                        KC_H,     KC_J,     KC_K,    KC_L, KC_SCLN, RCL_RPRT,
      LSFT_LCB ,KC_Z    ,KC_X     ,KC_C     ,KC_V     ,KC_B     ,KC_EQL ,KC_APP   ,   KC_INS,  KC_MINS,    KC_N,     KC_M,  KC_COMM,  KC_DOT, KC_SLSH, RSFT_RCB,
-                                  GO_MEDIA ,LALT_LBK ,GO_FUNCs ,KC_SPC ,KC_DEL   ,   KC_BSPC,  KC_ENT,GO_NUMBs, RALT_RBK,    KC_NO
+                                  KC_LGUI  ,LALT_LBK ,GO_FUNCs ,KC_SPC ,KC_DEL   ,   KC_BSPC,  KC_ENT,GO_NUMBs, RALT_RBK,  KC_RGUI
     ),
 
 /*
  * Base Layer: _NUMBERs
  *
  * ,---------------------------------------------.                              ,---------------------------------------------.
- * |   Tab   |  NO  |  $   |   #   | ` ~  |  |   |                              |  &   | ' "  |   *   |   %  |  NO  | Esc Gui |
+ * |   Tab   |  NO  |  $   |   #   | ` ~  |  |   |                              |  &   | ' "  |   *   |   %  |  NO  | Esc     |
  * |---------+------+------+-------+------+------|                              |------+------+-------+------+------+---------|
  * | Ctrl    | 1 !  | 2 @  |  3 #  | 4 $  | 5 %  |                              | 6 ^  | 7 &  |  8 *  | 9 (  | 0 )  | Ctrl    |
  * |---------+------+------+-------+------+------+-------------.  ,-------------+------+------+-------+------+------+---------|
  * | Shift   |  NO  |  Up  |  NO   | Down |  NO  | = +  | \ |  |  |  NO  | - _  |  NO  | Left |  NO   |Right |  NO  | Shift   |
  * `-----------------------+-------+------+------+------+------|  |------+------+------+------+-------+-----------------------'
- *                         |  NO   | LAlt | Alt  |Shift | Ctrl |  |  NO  |  NO  |(NUMB)| RAlt |   NO  |
+ *                         | L GUI | LAlt | Alt  |Shift | Ctrl |  |  NO  |  NO  |(NUMB)| RAlt | R GUI |
  *                         |       |  [   |      |      |      |  |      |      |      |   ]  |       |
  *                          -----------------------------------    -----------------------------------
 */
     [_NUMBERs] = LAYOUT(
-     KC_TAB   ,KC_NO   ,KC_DLR   ,KC_HASH  ,KC_GRV  ,KC_PIPE                   ,                  KC_AMPR,  KC_QUOT,  KC_ASTR, KC_PERC,   KC_NO,  KC_TRNS,
+     KC_TAB   ,KC_NO   ,KC_DLR   ,KC_HASH  ,KC_GRV  ,KC_PIPE                   ,                  KC_AMPR,  KC_QUOT,  KC_ASTR, KC_PERC,   KC_NO,   KC_ESC,
      KC_LCTL  ,KC_1    ,KC_2     ,KC_3     ,KC_4    ,KC_5                      ,                     KC_6,     KC_7,     KC_8,    KC_9,    KC_0,  KC_RCTL,
      KC_LSFT  ,KC_NO   ,KC_UP    ,KC_NO    ,KC_DOWN ,KC_NO   ,KC_EQL  ,KC_BSLS ,   KC_NO, KC_MINS,  KC_NO,  KC_LEFT,    KC_NO,KC_RIGHT,   KC_NO,  KC_RSFT,
-                                  KC_NO    ,KC_LALT ,KC_LALT ,KC_LSFT ,KC_LCTL ,   KC_NO,   KC_NO,KC_TRNS,  KC_RALT,    KC_NO
+                                  KC_LGUI  ,KC_LALT ,KC_LALT ,KC_LSFT ,KC_LCTL ,   KC_NO,   KC_NO,KC_TRNS,  KC_RALT,  KC_RGUI
     ),
 
 /*
  * Base Layer: _FUNCTIONs
  *
  * ,---------------------------------------------.                              ,---------------------------------------------.
- * |  Tab    |  NO  |  F9  |  F10  |  F12 |  NO  |                              |  NO  |  F4  |  F5   |  F6  |  NO  | Esc Gui |
+ * |  Tab    |  NO  |  F9  |  F10  |  F12 |  NO  |                              |  NO  |  F4  |  F5   |  F6  |  NO  | Esc     |
  * |---------+------+------+-------+------+------|                              |------+------+-------+------+------+---------|
  * | Ctrl    |  NO  |  F7  |  F8   |  F9  | PgUp |                              | Home |  F1  |  F2   |  F3  |  NO  | Ctrl    |
  * |---------+------+------+-------+------+------+-------------.  ,-------------+------+------+-------+------+------+---------|
- * | Shift   |  NO  |  Up  |  NO   | Down | PgDw |  NO  |NumLok|  |CapLok|  NO  | End  | Left |  NO   |Right |  NO  | Shift   |
+ * | Shift   |  NO  |  Up  |  NO   | Down | PgDw |  NO  |  NO  |  |CapLok|NumLok| End  | Left |  NO   |Right |  NO  | Shift   |
  * `-----------------------+-------+------+------+------+------|  |------+------+------+------+-------+-----------------------'
- *                         |  NO   | LAlt |(FUNC)|  NO  |  NO  |  | Ctrl |Shift |  Alt | RAlt |  NO   |
+ *                         | L GUI | LAlt |(FUNC)|  NO  |  NO  |  | Ctrl |Shift |  Alt | RAlt | R GUI |
  *                         |       |  [   |      |      |      |  |      |      |      |  ]   |       |
  *                          -----------------------------------    -----------------------------------
 */
     [_FUNCTIONs] = LAYOUT(
-     KC_TAB   ,KC_NO   ,KC_F10   ,KC_F11   ,KC_F12  ,KC_NO                     ,                     KC_NO,    KC_F4,    KC_F5,   KC_F6,   KC_NO,  KC_TRNS,
+     KC_TAB   ,KC_NO   ,KC_F10   ,KC_F11   ,KC_F12  ,KC_NO                     ,                     KC_NO,    KC_F4,    KC_F5,   KC_F6,   KC_NO,   KC_ESC,
      KC_LCTL  ,KC_NO   ,KC_F7    ,KC_F8    ,KC_F9   ,KC_PGUP                   ,                   KC_HOME,    KC_F1,    KC_F2,   KC_F3,   KC_NO,  KC_RCTL,
-     KC_LSFT  ,KC_NO   ,KC_UP    ,KC_NO    ,KC_DOWN ,KC_PGDN ,KC_NO   ,KC_NUM  , KC_CAPS,   KC_NO,  KC_END,  KC_LEFT,    KC_NO, KC_RGHT,   KC_NO,  KC_RSFT,
-                                  KC_NO    ,KC_LALT ,KC_TRNS ,KC_NO   ,KC_NO   , KC_LCTL, KC_LSFT, KC_LALT,  KC_RALT,    KC_NO
+     KC_LSFT  ,KC_NO   ,KC_UP    ,KC_NO    ,KC_DOWN ,KC_PGDN ,KC_NO   ,KC_NO   , KC_CAPS,  KC_NUM,  KC_END,  KC_LEFT,    KC_NO, KC_RGHT,   KC_NO,  KC_RSFT,
+                                  KC_LGUI  ,KC_LALT ,KC_TRNS ,KC_NO   ,KC_NO   , KC_LCTL, KC_LSFT, KC_LALT,  KC_RALT,  KC_RGUI
     ),
 
 /*
  * Base Layer: _MEDIA
  *
- * ,----------------------------------------------.                              ,---------------------------------------------.
- * |   LOCK  |  NO  |  NO  | VOLU  |  NO   |PAUSE |                              |RGB_F |  H_I |  S_I  | B_I  |  NO  |  SLEEP  |
- * |---------+------+------+-------+-------+------|                              |------+------+-------+------+------+---------|
- * |    NO   |  NO  | PREV | VOLD  | NEXT  | STOP |                              |RGB_R |  H_D |  S_D  | B_D  |  NO  |   NO    |
- * |---------+------+------+-------+-------+------+-------------.  ,-------------+------+------+-------+------+------+---------|
- * |    NO   |  NO  |  NO  | MUTE  |  NO   |LAUNCH|  NO  |  NO  |  |  NO  |  NO  |RGB_T |  NO  |  NO   |  NO  |  NO  |   NO    |
- * `-----------------------+-------+-------+------+------+------|  |------+------+------+------+-------+-----------------------'
- *                         |(MEDIA)|   NO  |  NO  |  NO  |  NO  |  |  NO  |  NO  |  NO  |  NO  |PWR_OFF|
- *                         |       |       |      |      |      |  |      |      |      |      |       |
- *                          ------------------------------------    -----------------------------------
+ * ,-----------------------------------------------.                               ,---------------------------------------------.
+ * | (MEDIA) |  NO  | PAUSE| VOLU  | STOP  |  NO   |                               |  NO   | TT_MM |  TL_T | TT_MX|  NO  |  LOCK   |
+ * |---------+------+------+-------+-------+-------|                               |-------+-------+-------+------+------+---------|
+ * |    NO   |  NO  | PREV | VOLD  | NEXT  |  NO   |                               |  NO   |  TL_L |  TL_B | TL_R |  NO  |  SLEEP  |
+ * |---------+------+------+-------+-------+-------+-------------.   ,-------------+-------+-------+-------+------+------+---------|
+ * |    NO   | NO   |LAUNCH| MUTE  |  NO   |  NO   |  NO  |  NO  |   |  NO  |  NO  |  NO   |  NO   |  NO   |  NO  |  NO  |  POWER  |
+ * `-----------------------+-------+-------+-------+------+------|   |------+------+-------+-------+-------+-----------------------'
+ *                         |WDW_MLS|WDW_MLW|GO_LWSP|  NO  |  NO  |   |  NO  |  NO  |GO_RWSP|WDW_MRW|WDW_MRS|
+ *                         |       |       |       |      |      |   |      |      |       |       |       |
+ *                          --------------------------------------    --------------------------------------
 */
     [_MEDIA] = LAYOUT(
-     LOCK  ,KC_NO   ,KC_NO    ,KC_VOLU ,KC_NO   ,KC_MPLY                   ,                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_SLEP,
-     KC_NO ,KC_NO   ,KC_MPRV  ,KC_VOLD ,KC_MNXT ,KC_MSTP                   ,                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-     KC_NO ,KC_NO   ,KC_NO    ,KC_MUTE ,KC_NO   ,KC_MSEL ,KC_NO   ,KC_NO   ,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                               KC_TRNS ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,   KC_NO,    KC_NO,   KC_NO,   KC_NO,  KC_PWR
-    ),
-
-/*
- * Base Layer: _NAVI
- *
- * ,-----------------------------------------------.                              ,-----------------------------------------------.
- * |    NO   |  NO  |  NO  |  NO   |  NO   |  NO   |                              |  NO   |TT_MM  |  TL_T | TT_MX|  NO  |   NO    |
- * |---------+------+------+-------+-------+-------|                              |-------+-------+-------+------+------+---------|
- * |    NO   |  NO  |  NO  |  NO   |  NO   |  NO   |                              |  NO   | TL_L  |  TL_B | TL_R |  NO  |   NO    |
- * |---------+------+------+-------+-------+-------+-------------.  ,-------------+-------+-------+-------+------+------+---------|
- * |    NO   |  NO  |  NO  |  NO   |  NO   |  NO   |  NO  |  NO  |  |  NO  |  NO  |  NO   |  NO   |  NO   |  NO  |  NO  |   NO    |
- * `-----------------------+-------+-------+-------+------+------|  |------+------+-------+-------+-------+-----------------------'
- *                         |WDW_MLS|WDW_MLW|GO_LWSP|  NO  |  NO  |  |  NO  |  NO  |GO_RWSP|WDW_MRW|WDW_MRS|
- *                         |       |       |       |      |      |  |      |      |       |       |       |
- *                          -------------------------------------    -------------------------------------
-*/
-    [_NAVI] = LAYOUT(
-     KC_TRNS ,KC_NO   ,KC_NO    ,KC_NO   ,KC_NO   ,KC_NO                     ,                      KC_NO,   TL_MM,    TL_T,   TL_MX,   KC_NO, KC_NO,
-     KC_NO   ,KC_NO   ,KC_NO    ,KC_NO   ,KC_NO   ,KC_NO                     ,                      KC_NO,    TL_L,    TL_B,    TL_R,   KC_NO, KC_NO,
-     KC_NO   ,KC_NO   ,KC_NO    ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
+     KC_TRNS ,KC_NO   ,KC_MPLY  ,KC_VOLU ,KC_MSTP ,KC_NO                     ,                      KC_NO,   TL_MM,    TL_T,   TL_MX,   KC_NO,    LOCK,
+     KC_NO   ,KC_NO   ,KC_MPRV  ,KC_VOLD ,KC_MNXT ,KC_NO                     ,                      KC_NO,    TL_L,    TL_B,    TL_R,   KC_NO, KC_SLEP,
+     KC_NO   ,KC_NO   ,KC_MSEL  ,KC_MUTE ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_PWR,
                                  WDW_MLS ,WDW_MLW ,GO_LWSP ,KC_NO   ,KC_NO   ,   KC_NO,    KC_NO, GO_RWSP, WDW_MRW, WDW_MRS
     ),
 // /*
@@ -263,9 +240,6 @@ bool oled_task_user(void) {
                 break;
             case _MEDIA:
                 oled_write_P(PSTR("Media keys\n"), false);
-                break;
-            case _MOUSE:
-                oled_write_P(PSTR("Mouse keys\n"), false);
                 break;
             default:
                 oled_write_P(PSTR("?\n"), false);
--- rules.mk
+++ rules.mk
@@ -1,7 +1,7 @@
 # COMMAND_ENABLE = no
 
 OLED_ENABLE = yes
-OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+OLED_DRIVER = ssd1306   # Enables the use of OLED displays
 # We are not going to use them, they have unsuable positions
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
